03beb2623d97c5e726cb222890e10682cabe697e
sunny <huiseon37@gmail.com>
2023-01-07 01:31:08 +0900

    feat: createSuggest 라우터 구현
    # —————————
    # init: 따단~~ 첫 커밋
    # feat: 새로운 기능, UI 구현
    # fix: 버그 수정,리포트
    # docs: readme 등의 문서 수정
    # edit: 파일 삭제 및 수정, 이동
    # del: 쓸모없는 코드 삭제
    # chore: 기능상 변경 없는 코드 수정, 패키지 관리 등
    # refactor: 완성된 기능 구조 변경, 리팩토링
    # —————————

--- src/router/salespostRouter.ts
+++ src/router/salespostRouter.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import { salespostController } from '../controller';
+import { auth, upload } from '../middlewares';
+
 const router: Router = Router();
+
+router.post(
+  '/:salespostId/suggest',
+  auth,
+  upload.single('image'),
+  salespostController.createSuggest,
+);
 
 export default router;
