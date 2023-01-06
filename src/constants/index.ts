a18196519bfd9031e043fb237877a8a670638809
sunny <huiseon37@gmail.com>
2023-01-07 03:17:00 +0900

    feat: 단어 리스트 생성
    # —————————
    # init: 따단~~ 첫 커밋
    # feat: 새로운 기능, UI 구현
    # fix: 버그 수정,리포트
    # docs: readme 등의 문서 수정
    # edit: 파일 삭제 및 수정, 이동
    # del: 쓸모없는 코드 삭제
    # chore: 기능상 변경 없는 코드 수정, 패키지 관리 등
    # refactor: 완성된 기능 구조 변경, 리팩토링
    # —————————

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as rm } from './responseMessage';
 export { default as sc } from './statusCode';
 export { default as tokenType } from './tokenType';
+export { default as wordList } from './wordList';
