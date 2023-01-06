73de1ae01dba950434c251e18a7d0ce3c6eea6f5
sunny <huiseon37@gmail.com>
2023-01-07 01:30:08 +0900

    chore: 패키지 수정
    # —————————
    # init: 따단~~ 첫 커밋
    # feat: 새로운 기능, UI 구현
    # fix: 버그 수정,리포트
    # docs: readme 등의 문서 수정
    # edit: 파일 삭제 및 수정, 이동
    # del: 쓸모없는 코드 삭제
    # chore: 기능상 변경 없는 코드 수정, 패키지 관리 등
    # refactor: 완성된 기능 구조 변경, 리팩토링
    # —————————

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,4 @@
 export { default as userController } from './userController';
 export { default as authController } from './authController';
 export { default as salespostController } from './salespostController';
-export { default as suggestontroller } from './suggestController';
+export { default as suggestController } from './suggestController';
