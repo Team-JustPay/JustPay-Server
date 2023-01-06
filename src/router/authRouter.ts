06f04e35f4db9ea7ae0ce8cd0cf7cc3c14b7d2d1
HoYoungChun <ho723@naver.com>
2023-01-06 22:17:56 +0900

    feat: move login to auth

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import { authController } from '../controller';
+
 const router: Router = Router();
+
+router.post('/login', authController.signInUser);
 
 export default router;
