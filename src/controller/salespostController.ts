6825a0af26328af901f05de8a19f6e63e9b9aa4d
sunny <huiseon37@gmail.com>
2023-01-07 01:31:27 +0900

    feat: createSuggest 컨트롤러 구현
    # —————————
    # init: 따단~~ 첫 커밋
    # feat: 새로운 기능, UI 구현
    # fix: 버그 수정,리포트
    # docs: readme 등의 문서 수정
    # edit: 파일 삭제 및 수정, 이동
    # del: 쓸모없는 코드 삭제
    # chore: 기능상 변경 없는 코드 수정, 패키지 관리 등
    # refactor: 완성된 기능 구조 변경, 리팩토링
    # —————————

--- src/controller/salespostController.ts
+++ src/controller/salespostController.ts
@@ -1,3 +1,31 @@
-const salespostController = {};
+import { Request, Response } from 'express';
+
+import { rm, sc } from '../constants';
+import { fail, success } from '../constants/response';
+import { SuggestCreateDTO } from '../interfaces/salespost/suggestCreateDTO';
+import { salespostService } from '../service';
+
+const createSuggest = async (req: Request, res: Response) => {
+  const { salespostId } = req.params;
+  const { userId } = res.locals;
+  const suggestCreateDTO: SuggestCreateDTO = req.body;
+  const image: Express.MulterS3.File = req.file as Express.MulterS3.File;
+  const location = image ? image.location : '';
+
+  const data = await salespostService.createSuggest(
+    userId,
+    Number(salespostId),
+    suggestCreateDTO,
+    location,
+  );
+
+  if (!data) {
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.CREATE_SUGGEST_FAIL));
+  }
+
+  return res.status(sc.CREATED).send(success(sc.CREATED, rm.CREATE_SUGGEST_SUCCESS, data));
+};
+
+const salespostController = { createSuggest };
 
 export default salespostController;
