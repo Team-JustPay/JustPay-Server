770afb7e953126ff71613cc08e7e62e08f840780
HoYoungChun <ho723@naver.com>
2023-01-07 18:09:33 +0900

    chore: response fail change

--- src/controller/salespostController.ts
+++ src/controller/salespostController.ts
@@ -66,13 +66,13 @@ const statusChange = async (req: Request, res: Response) => {
   const { salespostId } = req.params;
   const status = req.body.status;
   if (!(status in [0, 1])) {
-    return res.status(sc.BAD_REQUEST).send(success(sc.OK, rm.STATUS_NUMBER_ERROR));
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.STATUS_NUMBER_ERROR));
   }
 
   const data = await salespostService.statusChange(+salespostId, status);
 
   if (!data) {
-    return res.status(sc.NOT_FOUND).send(success(sc.NOT_FOUND, rm.STATUS_FAIL));
+    return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.STATUS_FAIL));
   }
   return res.status(sc.OK).send(success(sc.OK, rm.STATUS_CHANGE));
 };
@@ -83,13 +83,13 @@ const getPurchaseList = async (req: Request, res: Response) => {
   const { userId } = res.locals;
 
   if (!salespostId || !isMatched) {
-    return res.status(sc.BAD_REQUEST).send(success(sc.BAD_REQUEST, rm.GET_SUGGEST_LIST_FAIL));
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.GET_SUGGEST_LIST_FAIL));
   }
 
   const data = await salespostService.getPurchaseList(userId, +salespostId, isMatched as string);
 
   if (!data) {
-    return res.status(sc.NOT_FOUND).send(success(sc.NOT_FOUND, rm.GET_SUGGEST_LIST_FAIL));
+    return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.GET_SUGGEST_LIST_FAIL));
   }
 
   return res.status(sc.OK).send(success(sc.OK, rm.GET_SUGGEST_LIST_SUCCESS, data));
