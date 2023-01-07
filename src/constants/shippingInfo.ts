079534131d6c11eed43bfc04268eb1f92ffcb0f3
HoYoungChun <ho723@naver.com>
2023-01-07 21:57:25 +0900

    shipppingoption interface add

--- src/constants/shippingInfo.ts
+++ src/constants/shippingInfo.ts
@@ -1,4 +1,6 @@
-export const shippingContents: { [key: string]: string[] } = {
+import { shippingContentsType } from '../interfaces/type/shippingInfo';
+
+export const shippingContents: shippingContentsType = {
   반값택배: ['3-5일 소요'],
   끼리택배: ['4-6일 소요'],
   일반우편: ['3-4일 소요', '분실위험'],
