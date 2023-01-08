477497642c00f4dbc2284d4eef2cf328bd085dd2
sunny <huiseon37@gmail.com>
2023-01-09 00:22:58 +0900

    feat: date parser 형식 변경
    # —————————
    # init: 따단~~ 첫 커밋
    # feat: 새로운 기능, UI 구현
    # fix: 버그 수정,리포트
    # docs: readme 등의 문서 수정
    # edit: 파일 삭제 및 수정, 이동
    # del: 쓸모없는 코드 삭제
    # chore: 기능상 변경 없는 코드 수정, 패키지 관리 등
    # refactor: 완성된 기능 구조 변경, 리팩토링
    # —————————

--- src/modules/date.ts
+++ src/modules/date.ts
@@ -1,7 +1,8 @@
 const dateParser = (inputDate: Date) => {
   const year = inputDate.getFullYear();
-  const month = inputDate.getMonth() + 1;
-  const date = inputDate.getDate();
+  const month =
+    inputDate.getMonth() + 1 < 10 ? '0' + (inputDate.getMonth() + 1) : inputDate.getMonth() + 1;
+  const date = inputDate.getDate() < 10 ? '0' + inputDate.getDate() : inputDate.getDate();
   const weekdayMatch = ['일', '월', '화', '수', '목', '금', '토'];
   const weekday = weekdayMatch[inputDate.getDay()];
   const parsedDate = year + '.' + month + '.' + date + ' ' + '(' + weekday + ')';
