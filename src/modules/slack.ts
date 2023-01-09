63bb9f8fc88e2392e1b0bdb80a916481b85b5104
HoYoungChun <ho723@naver.com>
2023-01-09 23:34:10 +0900

    feat: add slack message to token invalid

--- src/modules/slack.ts
+++ src/modules/slack.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import config from '../config';
 
-export const sendSlackMessage = async (message: string) => {
+const sendSlackMessage = async (message: string) => {
   const res = await axios.post(config.webHookUrl, { text: message });
   return res.status === 200; // 성공헀는지 유무 전달
 };
+
+export default sendSlackMessage;
