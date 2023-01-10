be5059d78913a77acc36585fc946e6cca0d1b300
sunny <huiseon37@gmail.com>
2023-01-10 23:36:40 +0900

    test: 구매 제시 내용 조회 테스트 코드 작성

--- tests/suggests.test.ts
+++ tests/suggests.test.ts
@@ -1,24 +1,36 @@
 const request = require('supertest');
+import jwtHandler from '../src/modules/jwtHandler';
 require('dotenv').config();
 
 const app = require('../src/server.ts');
-
 let server = app.listen(4000);
+
+const token = jwtHandler.sign(4);
 
 afterAll(async () => {
   await server.close();
 });
 
 describe('suggests 라우터 테스트', ()=>{
-  describe('인증코드 생성 [GET]~/salesposts/certificationWord', () => {
-    test('200 - 일간 계획블록 리스트 조회 성공', async () => {
+  describe('구매 제시 내용 조회 [GET]~/suggests/:suggestId', () => {
+    test('200 - 구매 제시 내용 조회 성공', async () => {
       await request(app)
-      .get('/salesposts/certificationWord')
+      .get('/suggests/14')
       .set('Content-Type', 'application/json',)
+      .set('Authorization', `Bearer ${token}`)
       .expect(200)
+      .expect('Content-Type', /json/);
+    });
+
+    test('404 - 구매 제시 내용 조회 실패', async () => {
+      await request(app)
+      .get('/suggests/0')
+      .set('Content-Type', 'application/json',)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(404)
       .expect('Content-Type', /json/);
     });
   });
 });
 
-export {};
+export {}
