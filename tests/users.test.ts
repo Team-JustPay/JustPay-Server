b807d169a2947a98303a594e81b5debd0e449c04
sunny <huiseon37@gmail.com>
2023-01-10 19:16:14 +0900

    feat: 테스트 코드 파일 내부 세팅

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -1,8 +1,24 @@
-describe('계산 테스트', () => {
-  const a = 1,
-    b = 2;
+const request = require('supertest');
+require('dotenv').config();
 
-  test('a + b는 3이다.', () => {
-    expect(a + b).toEqual(3);
+const app = require('../src/server.ts');
+
+let server = app.listen(4000);
+
+afterAll(async () => {
+  await server.close();
+});
+
+describe('users 라우터 테스트', ()=>{
+  describe('인증코드 생성 [GET]~/salesposts/certificationWord', () => {
+    test('200 - 일간 계획블록 리스트 조회 성공', async () => {
+      await request(app)
+      .get('/salesposts/certificationWord')
+      .set('Content-Type', 'application/json',)
+      .expect(200)
+      .expect('Content-Type', /json/);
+    });
   });
 });
+
+export {};
