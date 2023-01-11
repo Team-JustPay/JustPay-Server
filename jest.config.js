d2c31cc6de7a3d04c2f595cbff786b1fffe30559
sunny <huiseon37@gmail.com>
2023-01-12 02:25:54 +0900

    chore: jest 설정 변경

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.export = {
   preset: "ts-jest", // ts-jest를 사용
   testEnvironment: "node", // 테스트 환경 'node' 환경을 사용
 	testMatch: ["**/__test__/*.test.(ts|tsx)"], //js 파일은 dist에서도 감지가 될 수 있으니 폴더를 조정해서 test이 있는 위치로 잡아준다.
-  setupFilesAfterEnv: ['<rootDir>/singleton.ts'],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true
+    }
+  }
 };
